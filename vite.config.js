61bd1e2300c01e81ca5c67ed19a38d6fe94dc0d0
akhilmukkara <akhil.mukkara@gmail.com>
2025-10-02 15:18:36 +0530

    removed everything for a fresh clutter-free start

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/lockedin-landing/', // Replace with your repo name
-});
